ad5e66386a18e137423b2181199d6c9b318bdf30
John P. Parlato <jparlato@mindspring.com>
2021-01-03 19:37:23 -0500

    addm mongo db to recipe app

--- node-course/notes-app/apps.js
+++ node-course/notes-app/apps.js
@@ -2,3 +2,7 @@ const getNotes = require("./notes.js");
 const chalk = require("chalk");
 
 console.log(chalk.green("success"));
+
+console.log(chalk.green("success"));
+
+console.log(chalk.bold.inverse.green("success"));
