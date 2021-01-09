ed362cd104452c87af427c32215d0213825b1cc2
John P. Parlato <jparlato@mindspring.com>
2021-01-09 17:02:47 -0500

    added mongoose and accessed databse after  connecting

--- confetti_cuisine/main.js
+++ confetti_cuisine/main.js
@@ -4,20 +4,41 @@ const express = require("express"),
   errorController = require("./controllers/errorController"),
   layouts = require("express-ejs-layouts");
 
-const MongoDB = require("mongodb").MongoClient,
-  dbURL = "mongodb://localhost:27017",
-  dbName = "recipe_db";
-
-MongoDB.connect(dbURL, (error, client) => {
-  if (error) throw error;
-  let db = client.db(dbName);
-  db.collection("contacts")
-    .find()
-    .toArray((error, data) => {
-      if (error) throw error;
-      console.log(data);
-    });
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/recipe_db", {
+  useNewUrlParser: true,
 });
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log(
+    "MongoDB connection error. Please make sure MongoDb is running.",
+    err
+  );
+  process.exit();
+});
+
+db.once("open", function () {
+  console.log("MongoDB running");
+}).on("error", function (err) {
+  console.log(err);
+});
+
+// const MongoDB = require("mongodb").MongoClient,
+//   dbURL = "mongodb://localhost:27017",
+//   dbName = "recipe_db";
+
+// MongoDB.connect(dbURL, (error, client) => {
+//   if (error) throw error;
+//   let db = client.db(dbName);
+//   db.collection("contacts")
+//     .find()
+//     .toArray((error, data) => {
+//       if (error) throw error;
+//       console.log(data);
+//     });
+// });
 
 app.set("view engine", "ejs");
 app.use(layouts);
