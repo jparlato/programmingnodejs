52a2382c1310359c55fdf777c27f88187d339873
John P. Parlato <jparlato@mindspring.com>
2021-01-09 16:22:25 -0500

    access mongodb in main to show all contacts, create connection

--- confetti_cuisine/main.js
+++ confetti_cuisine/main.js
@@ -4,6 +4,21 @@ const express = require("express"),
   errorController = require("./controllers/errorController"),
   layouts = require("express-ejs-layouts");
 
+const MongoDB = require("mongodb").MongoClient,
+  dbURL = "mongodb://localhost:27017",
+  dbName = "recipe_db";
+
+MongoDB.connect(dbURL, (error, client) => {
+  if (error) throw error;
+  let db = client.db(dbName);
+  db.collection("contacts")
+    .find()
+    .toArray((error, data) => {
+      if (error) throw error;
+      console.log(data);
+    });
+});
+
 app.set("view engine", "ejs");
 app.use(layouts);
 app.use(express.static("public"));
--- node-course/notes-app/apps.js
+++ node-course/notes-app/apps.js
@@ -1,8 +1,14 @@
 const getNotes = require("./notes.js");
 const chalk = require("chalk");
 
-console.log(chalk.green("success"));
+const command = process.argv[2];
 
-console.log(chalk.green("success"));
+if (command === "add") {
+  console.log(chalk.yellow("Adding Note"));
+} else {
+  console.log(chalk.yellow(command));
+}
 
-console.log(chalk.bold.inverse.green("success"));
+// console.log(chalk.yellow(process.argv[2]));
+
+// console.log(chalk.bold.inverse.green("success"));
